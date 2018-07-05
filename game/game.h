b29bd542f30a61c19efffcd818e2b06c1db66d34
ThiagoFBastos <40869714+ThiagoFBastos@users.noreply.github.com>
2018-07-05 20:00:09 -0300

    Update game.h

--- game/game.h
+++ game/game.h
@@ -1,14 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
 
-extern int game_state;
-
 // Componentes
 
 SPRITE avatar;
 IMAGE background;
 
-void LoadResources(const char*);
+void LoadGameResources();
 void Game();
 
 #endif
