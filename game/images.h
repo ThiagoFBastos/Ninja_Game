b2046179f7a056dde5dd6ece56b61137f12a2e3b
ThiagoFBastos <40869714+ThiagoFBastos@users.noreply.github.com>
2018-07-05 19:47:08 -0300

    Update images.h

--- game/images.h
+++ game/images.h
@@ -4,8 +4,8 @@
 #define TILE_H 200 // trocar
 #define TILE_W 300 // trocar
 
-#define TILE_MIN 50
 
+#define TILE_MIN 50 // tamanho do menor quadrado do tile map
 
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_image.h"
@@ -16,7 +16,7 @@ typedef struct
 {
 	SDL_Rect rect, hitBox;
 	SDL_Texture* texture;
-	int spritePos;
+	int spritePos; // posicao do sprite na imagem
 
 }SPRITE;
 
@@ -31,14 +31,46 @@ struct
 {
 	int tilemap[TILE_H][TILE_W];
 	SDL_Texture *texture;
-
 }TILEMAP;
 
+/* Verifica colisao entre retangulos
+ * Intersects retorna 1 caso haja colisao senao retorna 0
+*/
+
 int Intersects(SDL_Rect, SDL_Rect);
+
+/* Carrega o mapa no formato de tile maps 
+ * Cada tile map e identificado por seu id que varia de 0 a n
+ * O primeiro parametro e o endereco do arquivo de texto que contem o mapa
+ * O segundo parametro e o endereco da imagem
+*/
+
 void LoadMap(const char*, const char*);
+
+/* Renderiza o mapa em forma de tile maps
+ * O primeiro parametro e a posicao da camera do jogo
+*/
+
 void RenderMap(SDL_Point);
+
+/* Renderiza uma imagem
+ * O primeiro parametro e a imagem com suas informacoes
+ * O segundo parametro e a posicao da camera
+*/
+
 void Render_Image(Image*, SDL_Point);
+
+/* Renderiza um sprite
+ * O primeiro parametro e o sprite com suas informacoes
+ * O segundo parametro e a posicao da camera
+*/
+
 void Render_Sprite(Sprite*, SDL_Point);
+
+/* Carrega uma textura
+ * O primeiro parametro e o endereco da imagem
+ * A funcao retorna um ponteiro para o tipo SDL_Texture
+*/
 
 SDL_Texture* LoadTexture(const char*);
 
