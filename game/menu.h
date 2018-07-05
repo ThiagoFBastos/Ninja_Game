94e3d34e1bb9239e461265d2833654c96f8e7f66
ThiagoFBastos <40869714+ThiagoFBastos@users.noreply.github.com>
2018-07-05 19:35:30 -0300

    Update menu.h

--- game/menu.h
+++ game/menu.h
@@ -1,13 +1,17 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include "images.h"
+
 // Componentes
 
 IMAGE background;
 IMAGE buttons[1]; // trocar
 
-
+// Carrega todos os componentes necessarios para o funcionamento do menu
 void LoadMenuResources();
+
+// Trata os eventos relacionados ao menu e renderiza os componentes presentes no mesmo 
 void Menu();
 
 #endif
