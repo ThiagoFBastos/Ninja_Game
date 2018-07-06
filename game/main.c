fe037707cc5ce1d5bebd2648c0aebe7370d32091
ThiagoFBastos <40869714+ThiagoFBastos@users.noreply.github.com>
2018-07-06 20:31:58 -0300

    Update main.c

--- game/main.c
+++ game/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+
 #include "game_state.h"
+
 #include "game.h"
 #include "credits.h"
 #include "menu.h"
@@ -20,7 +22,7 @@ int main()
 
 	freopen("log/log_err.txt", "w", stderr);
 
-	if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+	if(SDL_Init(SDL_INIT_EVERYTHING) == -1) 
 		fputs("SDL_Init\n", stderr);
 	else
 	{
@@ -28,74 +30,75 @@ int main()
 
 		window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
 
-		if(!window)
+		if(window)
 		{
-			fprintf(stderr, "SDL_CreateWindow: %s\n", SDL_GetError());
-			return 1;
-		}
+			render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-
-		render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-		if(!render)
-		{
-			fprintf(stderr, "SDL_CreateRenderer: %s\n", SDL_GetError());
-			return 1;
-		}
-		
-		int imgInitFlags = IMG_INIT_PNG | IMG_INIT_JPG;
-		
-		if(IMG_Init(imgInitFlags) != imgInitFlags)
-		{
-			fputs("IMG_Init Failed\n", stderr);
-			return 1;
-		}
-		
-		LoadResourcesMenu();
-
-		while(game_state != EXIT)
-		{
-			timestamp = SDL_GetTicks();
-
-			switch(game_state)
+			if(render)
 			{
-				case MENU: 
-					Menu();
-				break;
-					
-				case CREDITS:
-					Credits();
-				 break;
-					
-				case RUN:
-					Game(); 
-				break;
-					
-				case OPTIONS:
-					Options();
-				break;
-					
-				case RANK_LIST:
-					RankList();
-				break;
+				int imgInitFlags = IMG_INIT_PNG | IMG_INIT_JPG;
+			
+				if(IMG_Init(imgInitFlags) != imgInitFlags)
+					fputs("IMG_Init Failed\n", stderr);
+				else
+				{
 				
-				case USER_ADD:
-					UserAdd();
-				break;
+					LoadResourcesMenu();
+
+					while(game_state != EXIT)
+					{
+						timestamp = SDL_GetTicks();
+
+						switch(game_state)
+						{
+							case MENU: 
+								Menu();
+							break;
+								
+							case CREDITS:
+								Credits();
+							 break;
+								
+							case RUN:
+								Game(); 
+							break;
+								
+							case OPTIONS:
+								Options();
+							break;
+								
+							case RANK_LIST:
+								RankList();
+							break;
+							
+							case USER_ADD:
+								UserAdd();
+							break;
+						}
+
+						timestamp = FPS - SDL_GetTicks() + timestamp;
+
+						if(timestamp > 0) 
+							SDL_Delay(timestamp);
+					}	
+				}
+
+				SDL_DestroyRenderer(render);
 			}
+			else
+				fprintf(stderr, "SDL_CreateRenderer: %s\n", SDL_GetError());
 
-			timestamp = FPS - SDL_GetTicks() + timestamp;
-
-			if(timestamp > 0) 
-				SDL_Delay(timestamp);
+			SDL_DestroyWindow(window);
+			
+			IMG_Quit();
 		}
+		else
+			fprintf(stderr, "SDL_CreateWindow: %s\n", SDL_GetError());
 
-		SDL_DestroyRenderer(render);
-		SDL_DestroyWindow(window);
-		
-		IMG_Quit();
 		SDL_Quit();
 	}
+
+	fclose(stderr);
 
 	return 0;
 }
