bd3ce756620e871b4c943f596b6354bc9b116e72
ThiagoFBastos <40869714+ThiagoFBastos@users.noreply.github.com>
2018-07-06 21:02:02 -0300

    Update menu.c

--- game/menu.c
+++ game/menu.c
@@ -11,30 +11,43 @@ extern int game_state;
 void LoadMenuResources()
 {
 	background.texture = LoadTexture("");
-    TTF_font* font = TTF_OpenFont("m5x7.ttf",32);
+    	
+	font = TTF_OpenFont("m5x7.ttf",32);
 	
 	//Faltam as posições dos retangulos
 	
 	//Novo jogo label
 	strcpy(button[0].text, "Novo jogo");
-	button[0].color.r = button[0].g = button[0].b = button[0].a = 255;
+	button[0].color.r = 255; 
+	button[0].color.g = 255;
+	button[0].color.b = 255;
+	button[0].color.a = 255;
 	New_Text(font, &button[0]);
 	
 	//Opções label
 	
 	strcpy(button[1].text, "Opções");
-	button[1].color.r = button[1].g = button[1].b = button[1].a = 255;
+	button[1].color.r = 255; 
+	button[1].color.g = 255;
+	button[1].color.b = 255;
+	button[1].color.a = 255;
 	New_Text(font, &button[1]);
 	
 	//Ranking label
 	
 	strcpy(button[2].text, "Ranking");
-	button[2].color.r = button[2].g = button[2].b = button[2].a = 255;
+	button[2].color.r = 255; 
+	button[2].color.g = 255;
+	button[2].color.b = 255;
+	button[2].color.a = 255;
 	New_Text(font, &button[2]);
 	
 	//Creditos label
 	strcpy(button[3].text, "Creditos");
-	button[3].color.r = button[3].g = button[3].b = button[3].a = 255;
+	button[3].color.r = 255; 
+	button[3].color.g = 255;
+	button[3].color.b = 255;
+	button[3].color.a = 255;
 	New_Text(font, &button[3]);
 }
 
@@ -47,26 +60,53 @@ void Menu()
 		
 		switch(e.type)
 		{
-			case SDL_QUIT: break;
+			case SDL_QUIT:
+			
+				game_state = EXIT;
+
+				break;
 				
 			case SDL_MOUSEMOTION:
 				
-				 for(int i = 0; i < 4; i++)
-            	 {
+				for(int i = 0; i < 4; i++)
+            	{
+
                 	if(HasPoint(e.motion.x, e.motion.y, labels[i].bounds)
-						labels[i].color.r = labels[i].color.g = labels[i].color.b = 126;
-					else
-					   labels[i].color.r = labels[i].color.g = labels[i].color.b = 255;
-				  }   
+                	{
+                		if(!labels[i].selected)
+                		{
+	                		labels[i].selected = SDL_TRUE;
+							labels[i].color.r = 126;
+							labels[i].color.g = 126;
+							labels[i].color.b = 126;
+							SDL_DestroyTexture(labels[i].texture);
+							New_Text(render, font, &labels[i]);
+						}
+                	}
+					else if(labels[i].selected)
+					{
+					   labels[i].selected = SDL_FALSE;
+					   labels[i].color.r = 255;
+					   labels[i].color.g = 255;
+					   labels[i].color.b = 255;
+					   SDL_DestroyTexture(labels[i].texture);
+					   New_Text(render, font, &labels[i]);
+					}
+
+				 }   
 					   
 				  break;
 					   
 			case SDL_MOUSEBUTTONDOWN:
 					   
-					 if(HasPoint(e.button.x, e.button.y, labels[0].bounds)) game_state = RUN;
-					 else if(HasPoint(e.button.x, e.button.y, labels[1].bounds)) game_state = OPTIONS;
-					 else if(HasPoint(e.button.x, e.button.y, labels[2].bounds)) game_state = RANK_LIST;
-					 else if(HasPoint(e.button.x, e.button.y, labels[3].bounds)) game_state = CREDITS;
+				if(HasPoint(e.button.x, e.button.y, labels[0].bounds))
+					game_state = RUN;
+				else if(HasPoint(e.button.x, e.button.y, labels[1].bounds))
+					game_state = OPTIONS;
+				else if(HasPoint(e.button.x, e.button.y, labels[2].bounds))
+					game_state = RANK_LIST;
+				else if(HasPoint(e.button.x, e.button.y, labels[3].bounds))
+					game_state = CREDITS;
             }
 		}
 	}
@@ -76,9 +116,12 @@ void Menu()
 	SDL_RenderCopy(render, background.texture, NULL, NULL);
 					
     for(int i = 0; i < 4; i++)
-        SDL_RenderCopy(render, button[i].texture, &button[i].bounds, NULL);
+        SDL_RenderCopy(render, button[i].texture, NULL, &button[i].bounds);
 
 	SDL_RenderPresent(render);
+
+	if(game_state != MENU)
+		FreeMenuResources();
 }
 
 void FreeMenuResources()
